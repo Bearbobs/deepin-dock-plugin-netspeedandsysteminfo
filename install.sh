ccb5727a8ff97e326faceaca88ff76f9f8da4652
Anuj kapoor <kapoor.anuj0@gmail.com>
2019-10-09 11:06:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo rm -f /usr/lib/dde-dock/plugins/libHTYCMDU.so
-sudo cp `dirname $0`/libnetspeed.so /usr/lib/dde-dock/plugins/libnetspeed.so
+sudo cp `pwd`/libnetspeed.so /usr/lib/dde-dock/plugins/libnetspeed.so
 killall dde-dock
